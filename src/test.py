94ff6a67eda9e854ce6cba0943d4a99c350af665
Qohey <shimad.sh9@gmail.com>
2022-03-18 22:18:33 +0900

    プログレスバー表示の修正

--- src/test.py
+++ src/test.py
@@ -5,6 +5,7 @@ import glob
 import json
 import logging
 from pathlib import Path
+from copy import deepcopy
 
 import numpy as np
 import torch
@@ -101,8 +102,9 @@ class Tester:
         p, r, f1, mp, mr, map50, map, t0, t1 = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
         jdict, stats, ap, ap_class = [], [], [], []
         seen = 0
-        info = (("%12s" * 8) % ("Class", "Images", "Targets", "P", "R", "mAP@.5", "mAP@.5:.95", ""))
-        for batch_i, (img, targets, paths, shapes) in enumerate(tqdm(self.data_loader, desc=info, unit="img")):
+        logger.info(("%12s" * 8) % ("Class", "Images", "Targets", "P", "R", "mAP@.5", "mAP@.5:.95", ""))
+        pbar = tqdm(enumerate(self.data_loader), total=len(self.data_loader), unit="img")
+        for batch_i, (img, targets, paths, shapes) in pbar:
             img = img.to(self.device, non_blocking=True)
             img = img.half() if self.half else img.float()
             img /= 255.0
@@ -123,6 +125,7 @@ class Tester:
                 output = non_max_suppression(inf_out, conf_thres=conf_thresh, iou_thres=iou_thresh)
                 t1 += time_synchronized() - t
 
+            # Statistics per image
             for si, pred in enumerate(output):
                 labels = targets[targets[:, 0] == si, 1:]
                 num_labels = len(labels)
@@ -161,7 +164,7 @@ class Tester:
                                       "category_id": int(p[5]),
                                       "bbox": [round(x, 3) for x in b],
                                       "score": round(p[4], 5)})
-                                      
+
                 # Assign all predictions as incorrect
                 correct = torch.zeros(pred.shape[0], niou, dtype=torch.bool, device=self.device)
                 if num_labels:
@@ -170,8 +173,8 @@ class Tester:
                     target_boxes = xywh2xyxy(labels[:, 1:5]) * whwh
 
                     for cls in torch.unique(target_class_tensor):
-                        target_indeces = (cls == target_class_tensor).nonzero(as_tuple=False).view(-1)  # prediction indices
-                        predict_indeces = (cls == pred[:, 5]).nonzero(as_tuple=False).view(-1)           # target indices
+                        target_indeces = (cls == target_class_tensor).nonzero(as_tuple=False).view(-1)  # Target indices
+                        predict_indeces = (cls == pred[:, 5]).nonzero(as_tuple=False).view(-1)          # Prediction indices
 
                         if predict_indeces.shape[0]:
                             ious, i = box_iou(pred[predict_indeces, :4], target_boxes[target_indeces]).max(1)
@@ -193,6 +196,20 @@ class Tester:
                 f = os.path.join(self.save_dir, f"test_batch{batch_i}_pred.jpg")
                 plot_images(images=img, targets=output_to_target(output, width, height), paths=paths, fname=f, names=self.names)
 
+            tmp_stats = deepcopy([np.concatenate(x, 0) for x in zip(*stats)])
+            if len(tmp_stats) and tmp_stats[0].any():
+                p, r, ap, f1, ap_class = ap_per_class(*tmp_stats)
+                p, r, ap50, ap = p[:, 0], r[:, 0], ap[:, 0], ap.mean(1)
+                mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
+                nt = np.bincount(tmp_stats[3].astype(np.int64), minlength=self.num_class)
+            else:
+                nt = torch.zeros(1)
+            del tmp_stats
+
+            info = ("%12s" + "%12.3g" * 6 + "%12s") % ("all", seen, nt.sum(), mp, mr, map50, map, "")
+            pbar.set_description(info)
+        # End test
+
         stats = [np.concatenate(x, 0) for x in zip(*stats)]
         if len(stats) and stats[0].any():
             p, r, ap, f1, ap_class = ap_per_class(*stats, plot=self.plots, fname=os.path.join(self.save_dir, "precision-recall_curve.png"))
@@ -202,11 +219,9 @@ class Tester:
         else:
             nt = torch.zeros(1)
 
-        print_format = "%12s" + "%12.3g" * 6 + "%12s"
-        print(print_format % ("all", seen, nt.sum(), mp, mr, map50, map, ""))
-
         if self.verbose and self.num_class > 1 and len(stats):
             for i, c in enumerate(ap_class):
+                print_format = "%12s" + "%12.3g" * 6 + "%12s"
                 print(print_format % (self.names[c], seen, nt[c], p[i], r[i], ap50[i], ap[i]))
 
         t = tuple(x / seen * 1E3 for x in (t0, t1, t0 + t1)) + (self.img_size, self.img_size, self.batch_size)
