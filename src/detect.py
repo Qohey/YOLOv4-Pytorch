6a702434a9a437b6ffdeed9eb150901cee27f1da
Qohey <shimad.sh9@gmail.com>
2022-03-20 23:17:49 +0900

    判定に関する処理の追加

--- src/detect.py
+++ src/detect.py
@@ -1,4 +1,27 @@
 # -*- coding: utf-8 -*-
 
-# class Detector:
-#     def __init__(self):
+
+from models.models import Darknet
+from src.utils.utils import load_class_names
+from src.utils.torch_utils import select_device
+
+class Detector:
+    def __init__(self, opt):
+        self.cfg        = opt.cfg
+        self.names      = load_class_names(opt.names)
+        self.weights    = opt.weights
+        self.device     = opt.device
+        self.img_size   = opt.img_size
+        self.dont_show  = opt.dont_show
+        self.save_img   = opt.save_img
+        self.save_txt   = opt.save_txt
+
+
+    def setup(self):
+        self.model  = Darknet(self.cfg, self.img_size).cuda()
+        self.device = select_device(self.device)
+        self.half   = (self.device.type != "cpu")
+
+
+    def detect(self, conf_thresh=0.001, iou_thresh=0.6):
+        return
