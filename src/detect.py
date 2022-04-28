16b7cd2d1b626ccd00c49358ed7c9dfa0a71cc81
Qohey <shimad.sh9@gmail.com>
2022-04-28 15:49:16 +0900

    判定回りの機能分割

--- src/detect.py
+++ src/detect.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import os
 import logging
 import time
 
+import cv2
 import torch
 from numpy import random
 
 from src.models.models import Darknet, load_darknet_weights
 from src.utils.utils import load_class_names
-from src.utils.datasets import LoadImages
+
 from src.utils.torch_utils import select_device, time_synchronized
 from src.utils.general import non_max_suppression, scale_coords, xyxy2xywh, strip_optimizer
 from src.utils.plots import plot_one_box
@@ -25,10 +27,9 @@ class Detector:
         self.img_size    = opt.img_size
         self.conf_thresh = opt.conf_thresh
         self.iou_thresh  = opt.iou_thresh
-        self.dont_show   = opt.dont_show
-        self.save_img    = opt.save_img
         self.save_txt    = opt.save_txt
         self.augment     = opt.augment
+        self.colors      = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(self.names))]
 
 
     def setup(self):
@@ -44,44 +45,32 @@ class Detector:
             self.model.half()
 
 
-    def detect(self, input=""):
-        dataset = LoadImages(input, img_size=self.img_size, auto_size=64)
-        colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(self.names))]
-        t0 = time.time()
-        img = torch.zeros((1, 3, self.img_size, self.img_size), device=self.device)
-        _ = self.model(img.half() if self.half else img) if self.device.type != 'cpu' else None
+    def detect(self, path, img, im0s, vid_cap):
+        img = torch.from_numpy(img).to(self.device)
+        img = img.half() if self.half else img.float()
+        img /= 255.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+        # Inference
+        predictions = self.model(img, augment=self.augment)[0]
+        # Apply NMS
+        predictions = non_max_suppression(predictions, self.conf_thresh, self.iou_thresh)
 
-        for path, img, im0s, vid_cap in dataset:
-            img = torch.from_numpy(img).to(self.device)
-            img = img.half() if self.half else img.float()
-            img /= 255.0
-            if img.ndimension() == 3:
-                img = img.unsqueeze(0)
+        return predictions, img
 
-            # Inference
-            t1 = time_synchronized()
-            predictions = self.model(img, augment=self.augment)[0]
+    def visualize(self, detect, path, img, im0s):
+        s = ""
+        s += "%gx%g " % img.shape[2:]  # print string
+        if detect is not None and len(detect):
+            # Rescale boxes from img_size to im0 size
+            detect[:, :4] = scale_coords(img.shape[2:], detect[:, :4], im0s.shape).round()
+            # Print results
+            for c in detect[:, -1].unique():
+                n = (detect[:, -1] == c).sum()  # detections per class
+                s += '%g %ss, ' % (n, self.names[int(c)])  # add to string
+            # Write results
+            for *xyxy, conf, cls in detect:
+                label = '%s %.2f' % (self.names[int(cls)], conf)
+                plot_one_box(xyxy, im0s, label=label, color=self.colors[int(cls)], line_thickness=3)
 
-            # Apply NMS
-            predictions = non_max_suppression(predictions, self.conf_thresh, self.iou_thresh)
-            t2 = time_synchronized()
-            from icecream import ic
-            for i, detect in enumerate(predictions):
-                p, s, im0 = path, "", im0s
-                s += '%gx%g ' % img.shape[2:]  # print string
-                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]
-                if detect is not None and len(detect):
-                    # Rescale boxes from img_size to im0 size
-                    detect[:, :4] = scale_coords(img.shape[2:], detect[:, :4], im0.shape).round()
-
-                    # Print results
-                    for c in detect[:, -1].unique():
-                        n = (detect[:, -1] == c).sum()  # detections per class
-                        s += '%g %ss, ' % (n, self.names[int(c)])  # add to string
-
-                    # Write results
-                    for *xyxy, conf, cls in detect:
-                        label = '%s %.2f' % (self.names[int(cls)], conf)
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-
-                print("%sDone. (%3.fs)" % (s, t2 - t1))
+        return im0s
--- yolov4.py
+++ yolov4.py
@@ -3,8 +3,8 @@
 import os
 import sys
 import logging
-import glob
 
+import cv2
 import torch
 
 from src import train
@@ -12,6 +12,7 @@ from src.train import Trainer
 from src.detect import Detector
 from src.test import Tester
 from src.utils.option import Options
+from src.utils.datasets import LoadImages
 
 
 if __name__ == "__main__":
@@ -36,7 +37,19 @@ if __name__ == "__main__":
         detector = Detector(opt)
         detector.setup()
         input_data = os.path.abspath(opt.input)
-        detector.detect(input_data)
+        datasets = LoadImages(input_data, img_size=opt.img_size, auto_size=64)
+
+        from icecream import ic
+        for path, img, im0s, vid_cap in datasets:
+            predictions, img = detector.detect(path, img, im0s, vid_cap)
+            for detect in predictions:
+                visualized = detector.visualize(detect, path, img, im0s)
+                if not opt.dont_show:
+                    cv2.imshow(path, im0s)
+                    cv2.waitKey(0)
+                if opt.save_img:
+                    save_path = os.path.splitext(os.path.basename(path))[0] + "_detected.jpg"
+                    cv2.imwrite(save_path, im0s)
 
     elif opt.action == "test":
         if opt.study == "test":
